6b8ce49f19afbc86581bb1b302d12f4822d49fe7
nithya <nithya@nithis.in>
2025-03-27 19:24:54 +0530

    feat: Implement Custom Delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,15 @@
 export const stringCalculator = (numbers: string): number => {
   if (!numbers) return 0;
+
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const delimiterEnd = numbers.indexOf("\n");
+    delimiter = new RegExp(numbers.substring(2, delimiterEnd));
+    numbers = numbers.substring(delimiterEnd + 1);
+  }
+
   return numbers
-    .split(/,|\n/)
+    .split(delimiter)
     .map(Number)
     .reduce((sum, num) => sum + num, 0);
 };
