d7a337be97cb6acc337c8a100a5be576211bc3fb
nithya <nithya@nithis.in>
2025-03-27 19:13:38 +0530

    test: failing test cases to support for one and two numbers

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -4,4 +4,12 @@ describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(stringCalculator("")).toBe(0);
   });
+
+  test("should return the number itself for a single number", () => {
+    expect(stringCalculator("1")).toBe(1);
+  });
+
+  test("should return the sum of two numbers", () => {
+    expect(stringCalculator("1,2")).toBe(3);
+  });
 });
