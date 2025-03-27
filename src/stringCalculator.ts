fae55d35ff71a60d8b526956dd5e976a1caefa30
nithya <nithya@nithis.in>
2025-03-27 19:22:40 +0530

    feat: Implement Newline Support

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,7 @@
 export const stringCalculator = (numbers: string): number => {
   if (!numbers) return 0;
   return numbers
-    .split(",")
+    .split(/,|\n/)
     .map(Number)
     .reduce((sum, num) => sum + num, 0);
 };
